765e9abe7b2d5d1a2673899ea541d7bff4c3041f
Sayan Hazra <44938325+Sayan2919@users.noreply.github.com>
2020-10-01 14:24:44 +0530

    Update isArmstrong.py

--- HaacktoberFest 2019/isArmstrong.py
+++ HaacktoberFest 2019/isArmstrong.py
@@ -1,13 +1,17 @@
 def is_armstrong(n):
 	m=0
 	t=n
+	#flag = False
 	while n>0:
 		r=(n%10)
+		#m += r ** 3
 		m=m+(r*r*r)
 		n=n/10
 	print m
 	if m==t:
+		#flag = True
 		print "Yes it is armstrong"
-	else:
-		print "Not it is not armstrong"
+	'''else:
+		print "Not it is not armstrong"'''
+	return flag
 is_armstrong(153)
